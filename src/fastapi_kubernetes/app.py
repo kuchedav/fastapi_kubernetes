4df6e9cf0a3aaaef3ce735fdd606861acfcc0a31
kuchedav <kuchelmeister.david@gmail.com>
2024-04-23 16:21:26 +0200

    add authentication to my api

--- src/fastapi_kubernetes/app.py
+++ src/fastapi_kubernetes/app.py
@@ -1,8 +1,27 @@
+from daves_utilities.david_secrets import get_credentials
+from fastapi import Depends
 from fastapi import FastAPI
+from fastapi import HTTPException
+from fastapi import status
+from fastapi.security import HTTPBasic
+from fastapi.security import HTTPBasicCredentials
 
 app = FastAPI()
+security = HTTPBasic()
 
 
-@app.get("/")
+def authenticate_user(credentials: HTTPBasicCredentials = Depends(security)):
+    crd = get_credentials("fastapi")
+
+    if credentials.username != crd["usr"] or credentials.password != crd["pwd"]:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
+
+@app.get("/", dependencies=[Depends(authenticate_user)])
 async def root():
     return {"message": "Hello World"}
--- tests/test_default.py
+++ tests/test_default.py
@@ -1,4 +1,5 @@
 import pytest
+from daves_utilities.david_secrets import get_credentials
 from fastapi.testclient import TestClient
 
 from fastapi_kubernetes.app import app
@@ -10,6 +11,12 @@ def client():
 
 
 def test_api_output(client):
-    response = client.get("/")
+    crd = get_credentials("fastapi")
+    response = client.get("/", auth=(crd["usr"], crd["pwd"]))
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    ...
+    test_api_output(client())
